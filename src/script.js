0f656ea377ea28bb3867820c6e7fea2eb315c42c
Cheburek254 <kirden06122007@gmail.com>
2024-04-03 19:11:28 +0300

    add history

--- src/script.js
+++ src/script.js
@@ -5,6 +5,9 @@ let history_block = document.querySelector('.history-for-js')
 let crafts_block = document.querySelector('.craft-to-js')
 let choose_block = document.querySelector('.choose-for-js')
 let crafts = document.querySelector('.crafts')
+let cookies = document.cookie
+cookies = cookies.split(';')
+let his = document.querySelector('.his')
 //let receips = {
 //   название предмета :    [imageurl,[список необходимых предмктов(0  если в этой ячейке предмета нет)]]
 //}
@@ -26,22 +29,20 @@ let receips = {"bubu" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile0
 "bubuo" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]],
 "bubui" : ["img/craftable/tile004.png",[0,0,0,"img/craftable/tile004.png","img/craftable/tile004.png","img/craftable/tile004.png",0,0,0]]
 }
-btn_history.addEventListener('click',function(){
-    history_block.style.display = 'flex'
-    crafts_block.style.display = 'none'
-    choose_block.style.display = 'none'
-})
+
 
 btn_all.addEventListener('click',function(){
     crafts_block.style.display = 'flex'
     history_block.style.display = 'none'
     choose_block.style.display = 'none'
+    his.innerHTML = ''
 })
 
 btn_choose.addEventListener('click',function(){
     choose_block.style.display = 'flex'
     crafts_block.style.display = 'none'
     history_block.style.display = 'none'
+    his.innerHTML = ''
 })
 
 let a = crafts.querySelector('.row')
@@ -51,33 +52,87 @@ Object.keys(receips).forEach(key => {
     a.innerHTML+=`<div class="icon spa"><img class="block-img" src="${receips[key][0]}"></div>`
         
 })
+
+let hist = ''
+let histl = []
+for (let i = 0;i < cookies.length; i++){
+    let cookie = cookies[i]
+    let o = cookie.split('=')
+    if  (o[0]=='history'){
+      hist = o[1]
+      histl = hist.split(',')
+    }
+}
+
+
+
+
+
+
+
+
+
+
 let items = document.querySelectorAll('.spa')
 let objects = Object.keys(receips)
 let open = document.querySelector('.open')
-console.log(objects,objects.length)
+
 for (let i = 0;i<objects.length;i++){
     items[i].addEventListener('click',function(){
         open.innerHTML = `<div class="left">
-        <div class="row1">
+        
           <img class="block-img" src="${receips[objects[i]][1][0]}" alt = "">
           <img class="block-img" src="${receips[objects[i]][1][1]}" alt = "">
           <img class="block-img" src="${receips[objects[i]][1][2]}" alt = "">
-        </div>
-        <div class="row1">
+        
           <img class="block-img" src="${receips[objects[i]][1][3]}" alt = "">
           <img class="block-img" src="${receips[objects[i]][1][4]}" alt = "">
           <img class="block-img" src="${receips[objects[i]][1][5]}" alt = "">
-        </div>
-        <div class="row1">
+        
           <img class="block-img" src="${receips[objects[i]][1][6]}" alt = "">
           <img class="block-img" src="${receips[objects[i]][1][7]}" alt = "">
           <img class="block-img" src="${receips[objects[i]][1][8]}" alt = "">
-        </div>
+        
       </div>
       <div class="arrow"><img src="img/arrow.png"></div>
       <div class="icon"><img class="block-img" src="${receips[objects[i]][0]}"></div>
         `
-        console.log(i)
+      if(hist.length == 0){
+        document.cookie = `history=${objects[i]};max-age=10000000000`
+        histl.push(objects[i])
+      }  
+      else{
+        document.cookie = `history=${histl.join(',')+',' + objects[i]};max-age=10000000000`
+        histl.push(objects[i])
+      }
 
     })
 }
+btn_history.addEventListener('click',function(){
+  history_block.style.display = 'flex'
+  crafts_block.style.display = 'none'
+  choose_block.style.display = 'none'
+  histl.forEach(element =>{
+    his.innerHTML+= `<div class="craft hh">
+    <div class="left">
+        
+    <img class="block-img" src="${receips[element][1][0]}" alt = "">
+    <img class="block-img" src="${receips[element][1][1]}" alt = "">
+    <img class="block-img" src="${receips[element][1][2]}" alt = "">
+  
+    <img class="block-img" src="${receips[element][1][3]}" alt = "">
+    <img class="block-img" src="${receips[element][1][4]}" alt = "">
+    <img class="block-img" src="${receips[element][1][5]}" alt = "">
+  
+    <img class="block-img" src="${receips[element][1][6]}" alt = "">
+    <img class="block-img" src="${receips[element][1][7]}" alt = "">
+    <img class="block-img" src="${receips[element][1][8]}" alt = "">
+  
+</div>
+<div class="arrow"><img src="img/arrow.png"></div>
+<div class="icon"><img class="block-img" src="${receips[element][0]}"></div>
+</div>
+  `
+  })
+  
+})
